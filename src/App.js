3d106546e5ae7a573898cfc590205d64e677a6e9
Pooja-287 <pooja28.vdl@gmail.com>
2025-06-14 12:01:02 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+
+// // Import Home component
+// import Header from './components/filter/Header';
+// import Home from './components/home/Home';
+
+// import FilterOption from './components/filter/FilterOption';
+
+// function App() {
+//   return (
+//     <>
+//     <main className = 'container-fluid'>
+//      <Home/>
+//      <Header/>
+//      <FilterOption/>
+//     </main>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+import { Route, Routes,} from "react-router-dom";
+import QuickSearch from "./components/filter/QuickSearch";
+import Home from "./components/home/Home";
+import Restaurant from "./components/restaurant/Restaurant";
+
+
+
+function App() {
+  return (
+    <>
+      <main className="container-fluid">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quick-search/:meal_id" element={<QuickSearch />} />
+          <Route path="/restaurant/:id" element={<Restaurant/>} />
+        
+        </Routes>
+        {/* <Home/>
+        <QuickSearch/> */}
+      </main>
+    </>
+  );
+} 
+
+export default App;
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// import './css/index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
+
+import "./css/index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
